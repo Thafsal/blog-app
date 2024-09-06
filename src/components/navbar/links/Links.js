b2303880d2bf0763716aaa5ad3cc8eebf4e46e04
Thafsal <thafsalthafsu@gmail.com>
2024-09-06 20:08:45 +0530

    Created "Media queries for various devices has created"

--- src/components/navbar/links/Links.js
+++ src/components/navbar/links/Links.js
@@ -1,3 +1,5 @@
+"use client"
+import { useState } from "react";
 import styles from "./links.module.css";
 import { NavLinks } from "./navLinks/NavLinks";
 
@@ -13,21 +15,57 @@ const link_name = [
   },
 
   {
-    title: "Contact",
-    path: "/contact",
-  },
-  {
     title: "Blogs",
     path: "/blog",
   },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
 ];
 
+const session = true
+const isAdmin = true
+
 const Links = () => {
-  return <div className={styles.links}>
-    {link_name.map((link) => (
-      <NavLinks item={link} key={link.title} />
-    ))}
-  </div>;
+  const [open,setIsOpen] = useState(false)
+  return (
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {link_name.map((link) => (
+          <NavLinks item={link} key={link.title} />
+        ))}
+        {
+          session?(
+            <>
+            {isAdmin&& <NavLinks item={{title:"Admin", path:"/admin"}} />}
+            <button className={styles.logout_btn}>Logout</button>
+            </>
+          ):(
+            <NavLinks item={{title:"Login", path:"/login"}} />
+          )
+        }
+      </div>
+      <button className={styles.menutoggle} onClick={() => setIsOpen((prev) => !prev)}>Menu</button>
+      {
+        open&& <div className={styles.mobileLinks}>
+          {link_name.map((link) => (
+            <NavLinks item={link} key={link.title} />  
+          ))}
+          {
+          session?(
+            <>
+            {isAdmin&& <NavLinks item={{title:"Admin", path:"/admin"}} />}
+            <button className={styles.logout_btn}>Logout</button>
+            </>
+          ):(
+            <NavLinks item={{title:"Login", path:"/login"}} />
+          )
+        }
+        </div>
+      }
+    </div>
+  )
 };
 
 export default Links;
